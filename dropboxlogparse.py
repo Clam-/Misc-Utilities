1f8e5680f2da054cc000ef4603aef98525d04594
Lee <clam@nyanya.org>
2018-10-26 17:25:09 +1100

    Update dropboxlogparse.py

--- dropboxlogparse.py
+++ dropboxlogparse.py
@@ -13,22 +13,28 @@ infilename = sys.argv[1]
 reader = csv.reader(open(infilename,'rb'), dialect='excel', encoding='utf-8')
 writer = csv.writer(open(infilename+"-output.csv","wb"), dialect='excel', encoding='utf-8')
 firstrow = True
+rownum = -1
 for row in reader:
-    if firstrow:
+    rownum += 1
+    if rownum == 0:
         for counter,field in enumerate(row):
             COLTITLES[field] = counter
             COLNUMBER += 1
-        firstrow = False
         continue
     # row 9 JSON
-    wrow = row[:9] + [""]*100
-    d = json.loads(row[9])
-    for key in d:
-        if key not in COLTITLES:
-            COLTITLES[key] = COLNUMBER
-            COLNUMBER += 1
-        if key in COLTITLES:
-            wrow[COLTITLES[key]] = d[key]
+    wrow = row[:9] + [""]*150
 
+    try:
+        d = json.loads(row[9])
+        for key in d:
+            if key not in COLTITLES:
+                COLTITLES[key] = COLNUMBER
+                COLNUMBER += 1
+            if key in COLTITLES:
+                wrow[COLTITLES[key]] = d[key]
+    except ValueError as ex:
+        wrow = row
+        print "ERROR ON ROW: %s" % rownum
+        print ex
     writer.writerow(wrow)
 writer.writerow(sorted(COLTITLES, key=lambda k: COLTITLES[k]))
