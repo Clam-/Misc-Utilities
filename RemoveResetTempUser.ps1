92f2856a468c2dec1d0c90c6da19c4ee9815bb4d
Clam- <clam@nyanya.org>
2022-09-15 14:08:57 +1000

    Update RemoveResetTempUser.ps1

--- RemoveResetTempUser.ps1
+++ RemoveResetTempUser.ps1
@@ -17,12 +17,30 @@ function Remove-LocalUserCompletely {
         Get-CimInstance -Class Win32_UserProfile | ? SID -eq $user.SID | Remove-CimInstance
     }
 }
+function LogOffUser {
+    Param(
+        [Parameter(ValueFromPipelineByPropertyName)]
+        $Name
+    )
+
+    process {
+        #https://stackoverflow.com/a/49042770
+        $sessions = (((quser) -replace '^>', '') -replace '\s{2,}', ',' | ConvertFrom-Csv) | Where-Object {$_.USERNAME -eq 'banh user'}
+        foreach ($session in $sessions)
+        {
+          logoff $session.SESSIONNAME
+        }
+    }
+}
 
 # Example usage:
 #Remove-LocalUserCompletely -Name 'myuser'
+# log off user
+LogOffUser
+
 Write-Host "Cleaning user..."
-Remove-LocalUserCompletely -Name 'BAHN'
+Remove-LocalUserCompletely -Name 'Banh User'
 Write-Host "Remaking user..."
-New-LocalUser -Name "BAHN" -Description "Description of this account." -NoPassword
+New-LocalUser -Name "Banh User" -Description "Description of this account." -NoPassword
 Write-Host "Done. Device is ready for new user. You can close this window now (it will automatically close in 30 seconds)"
 Start-Sleep -Seconds 30
